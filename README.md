c38d134d5d4fcaaf9b3805e6dabda5dc830701f4
Dada Khalandar <kerry.blig12@gmail.com>
2021-12-05 15:29:36 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,36 @@
+## Star Wars
+
+It's a simple web application to search and know about your favorite star wars characters info.
+
+<h2 align="center">
+  <a href="https://star-wars-characters-info.vercel.app/" target="_blank">Star Wars</a>
+</h2>
+
+<p align="center">
+  Sample Desktop Application 👇
+</p>
+
+<p align="center">
+<img src="https://github.com/kerrybli/portfolio/blob/master/src/assests/images/star-wars.png?raw=true">
+</p>
+
+## Built With
+
+This project was built using these technologies.
+
+- JavaScript
+- HTML
+- CSS
+- VsCode
+- Deployed on Vercel
+
+## Features are: 😀
+
+- Search bar to search your characters.
+- Mute and Un-mute Button.
+- New icons are added to the bottom taskbar.
+- Dark background with animation.
+
+### Show your support
+
+Give a ⭐ if you like this website! Fork it and contribute
--- star-wars.js
+++ star-wars.js
@@ -152,82 +152,81 @@ let goBack = () => {
 };
 
 particlesJS("particles-js", {
-    particles: {
-      number: {
-        value: 139,
-        density: { enable: true, value_area: 789.1476416322727 }
-      },
-      color: { value: "#ffffff" },
-      shape: {
-        type: "circle",
-        stroke: { width: 1, color: "#000000" },
-        polygon: { nb_sides: 5 },
-        image: { src: "img/github.svg", width: 100, height: 100 }
-      },
-      opacity: {
-        value: 0.7417987831343363,
-        random: true,
-        anim: { enable: true, speed: 1, opacity_min: 0, sync: false }
-      },
-      size: {
-        value: 3.945738208161363,
-        random: true,
-        anim: { enable: true, speed: 9.59040959040959, size_min: 0, sync: true }
-      },
-      line_linked: {
-        enable: false,
-        distance: 150,
-        color: "#ffffff",
-        opacity: 0.4,
-        width: 1
-      },
-      move: {
+  particles: {
+    number: {
+      value: 139,
+      density: { enable: true, value_area: 789.1476416322727 }
+    },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 1, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 }
+    },
+    opacity: {
+      value: 0.7417987831343363,
+      random: true,
+      anim: { enable: true, speed: 1, opacity_min: 0, sync: false }
+    },
+    size: {
+      value: 3.945738208161363,
+      random: true,
+      anim: { enable: true, speed: 9.59040959040959, size_min: 0, sync: true }
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 6.413648243462092,
+      direction: "bottom-right",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
         enable: true,
-        speed: 6.413648243462092,
-        direction: "bottom-right",
-        random: true,
-        straight: false,
-        out_mode: "out",
-        bounce: false,
-        attract: {
-          enable: true,
-          rotateX: 721.5354273894853,
-          rotateY: 962.0472365193136
-        }
+        rotateX: 721.5354273894853,
+        rotateY: 962.0472365193136
       }
-    },
-    interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: { enable: true, mode: "grab" },
-        onclick: { enable: false, mode: "remove" },
-        resize: true
-      },
-      modes: {
-        grab: { distance: 219.26084732136317, line_linked: { opacity: 1 } },
-        bubble: { distance: 250, size: 0, duration: 2, opacity: 0, speed: 3 },
-        repulse: { distance: 400, duration: 0.4 },
-        push: { particles_nb: 4 },
-        remove: { particles_nb: 2 }
-      }
-    },
-    retina_detect: true
-  });
-  var count_particles, stats, update;
-  stats = new Stats();
-  stats.setMode(0);
-  stats.domElement.style.position = "absolute";
-  stats.domElement.style.left = "0px";
-  stats.domElement.style.top = "0px";
-  document.body.appendChild(stats.domElement);
-  count_particles = document.querySelector(".js-count-particles");
-  update = function () {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
     }
-    requestAnimationFrame(update);
-  };
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: false, mode: "remove" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 219.26084732136317, line_linked: { opacity: 1 } },
+      bubble: { distance: 250, size: 0, duration: 2, opacity: 0, speed: 3 },
+      repulse: { distance: 400, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+});
+var count_particles, stats, update;
+stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = "absolute";
+stats.domElement.style.left = "0px";
+stats.domElement.style.top = "0px";
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector(".js-count-particles");
+update = function () {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
   requestAnimationFrame(update);
-  
+};
+requestAnimationFrame(update);
